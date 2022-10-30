2d2ef30d0d9d49b2325718f6e6ad74fd14ed1a5f
AshotHeb <58462105+AshotHeb@users.noreply.github.com>
2022-10-30 09:44:53 +0400

    Was fixed code after review

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,79 +1,157 @@
 import "./styles.scss"
-import {Card} from "../Card"
-import{Button} from "../../components/Button"
-export const Main=()=>{
-    return(
+import { Card } from "../Card"
+// import { Button } from "../../components/Button"
+
+export const Main = () => {
+    return (
         <div className="main-div">
-        
-            <div className="main-heading">
-                <h2 className="h2">Offerings open for investment</h2>
-                <p className="p">Explore pre-vetted investment opportunities available in a growing
-                </p>
-                <span className="p2"> number of industry categories.</span>
-            </div>
-            
-          <div className="card-wrapper">
-        <Card 
-        img={"/images/Img.png"} 
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        cont={"/images/Bar.svg"}
-        range={"$574,920 raised of $1,000,000"}
-        />
-        
-        <Card  
-       
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        range={"$574,920 raised of $1,000,000"}
-        />
-        
-        <Card  
-        img={"/images/Img.jpg"}
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        range={"$574,920 raised of $1,000,000"}
-        />
-        
-        <Card  
-        img={"/images/Img(3).png"}
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        range={"$574,920 raised of $1,000,000"}
-        />
-        
-        <Card  
-        img={"/images/Img.png"}
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        range={"$574,920 raised of $1,000,000"}
-        />
-        
-        <Card  
-        img={"/images/Img.png"}
-        name={"Oxalis"}
-        location={"Brooklyn, NY"}
-        description={"A recognized leader in language immersion & early education, opening second school."}
-        range={"$574,920 raised of $1,000,000"}
-        />
-</div>
-<div className="main-button">
-                <Button
-                    outline
-                    color="red"
-                >
-                    View All Projects
-                </Button>
+            <div className="main-div-content">
+                <div className="main-heading-section">
+                    <h3 className="main-heading-section-h2">
+                        Offerings open for investment
+                    </h3>
+                    <p className="main-heading-section-p ">
+                        Explore pre-vetted investment opportunities available in a growing
+                        number of industry categories.
+                    </p>
                 </div>
-</div>
-       
- 
-       
-       
+            </div>
+
+            <div className="card-wrapper">
+                <Card
+                    img={"/images/Img.png"}
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    cont={"/images/Bar.svg"}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+
+                <Card
+
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+
+                <Card
+                    img={"/images/Img.jpg"}
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+
+                <Card
+                    img={"/images/Img(3).png"}
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+
+                <Card
+                    img={"/images/Img.png"}
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+
+                <Card
+                    img={"/images/Img.png"}
+                    name={"Oxalis"}
+                    location={"Brooklyn, NY"}
+                    description={"A recognized leader in language immersion & early education, opening second school."}
+                    range={"$574,920 raised of $1,000,000"}
+                />
+            </div>
+
+        </div>
     )
 }
+
+
+
+/*Qo code*/
+// import "./styles.scss"
+// import { Card } from "../Card"
+// import { Button } from "../../components/Button"
+// export const Main = () => {
+//     return (
+//         <div className="main-div">
+
+//             <div className="main-heading">
+//                 <h2 className="h2">Offerings open for investment</h2>
+//                 <p className="p">
+//                     Explore pre-vetted investment opportunities available in a growing
+//                     number of industry categories.
+//                 </p>
+//             </div>
+
+//             <div className="card-wrapper">
+//                 <Card
+//                     img={"/images/Img.png"}
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     cont={"/images/Bar.svg"}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+
+//                 <Card
+
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+
+//                 <Card
+//                     img={"/images/Img.jpg"}
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+
+//                 <Card
+//                     img={"/images/Img(3).png"}
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+
+//                 <Card
+//                     img={"/images/Img.png"}
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+
+//                 <Card
+//                     img={"/images/Img.png"}
+//                     name={"Oxalis"}
+//                     location={"Brooklyn, NY"}
+//                     description={"A recognized leader in language immersion & early education, opening second school."}
+//                     range={"$574,920 raised of $1,000,000"}
+//                 />
+//             </div>
+//             <div className="main-button">
+//                 <Button
+//                     outline
+//                     color="red"
+//                 >
+//                     View All Projects
+//                 </Button>
+//             </div>
+//         </div>
+
+
+
+
+//     )
+// }
