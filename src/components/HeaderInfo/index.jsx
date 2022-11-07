a1c2a681639b812561fd78ac185126b6cecf0d1b
Arpi1988 <arpinehakobyan58@gmail.com>
2022-11-06 22:29:00 -0800

    change

--- src/components/HeaderInfo/index.jsx
+++ src/components/HeaderInfo/index.jsx
@@ -1,6 +1,20 @@
 import "./style.scss"
 export const HeaderInfo=()=>{
     return(
-        <div></div>
+        <div className="header-info-div">
+            <div className="stay-connected-section">
+                <div>New</div>
+                <div>Stay connected to the upcoming & Recent jobs</div>
+            </div>
+            
+            <h1 className="h1">Your Solution   Legal Consultacy</h1>
+            <p className="p1">We are here to help you take care of your legality with the best service especially for you.</p>
+            <button className="button"></button>
+            <p className="p2"></p>
+            <div className="group-25"></div>
+            <img src="images/girl.png" alt="img" className="girl-img" />
+            <div className="absolute-div"></div>
+            
+        </div>
     )
 }
--- src/components/HeaderNav/index.jsx
+++ src/components/HeaderNav/index.jsx
@@ -4,12 +4,14 @@ export const HeaderNav=()=>{
     return (
         <div className="header-nav-div">
             <img src="images/Logo.png" alt="img" />
-                    <ul className="ul">Lawliet
+                <ul className="ul">Lawliet
                 <li className="li">CONTACT</li>
                 <li className="li"> TESTIMONIALS</li>
                 <li className="li">ABOUT</li>
                 <li className="li">TERMS</li>
             </ul>
+            <img src="images/Search.png" alt="img" />
+           
         </div>
     )
 }
